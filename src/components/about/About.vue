f580470908012c037b1574a8eae197d56ca04632
NoranBESTEAM <nmohammed@be-steam.com>
2023-12-12 09:26:34 +0200

    About Component In About Page

---
+++ src/components/about/About.vue
@@ -0,0 +1,136 @@
+<template>
+    <section id="about">
+
+        <div class="container">
+
+            <div class="row">
+
+                <div class="col-12 col-sm-12 col-md-7 col-lg-7 col-xl-7">
+
+                    <div id="text_wrapper">
+
+                        <h3 class="fw-bold mb-4 text-justify">من نحن</h3>
+
+                        <h5 class="fw-bold mb-3 text-justify">شركة رائدة في خدمات STEAM التعليمية!</h5>
+
+                        <p class="text-justify">نعمل لتأهيل الطلاب في مهارات المستقبل القائمة على النهضة الصناعية الرابعة عن
+                            طريق توفير كل
+                            متطلبات الحصول على امتياز STEAM التجاري. نهدف إلى تمكين الجهات التعليمية والمدربين من تقديم بيئة
+                            تعليمية مستدامة ومبتكرة تعزز مهارات التطبيق العملي والإبداع والتعاون وحل المشكلات.</p>
+
+                    </div>
+
+                </div>
+
+                <div class="col-12 col-sm-12 col-md-5 col-lg-5 col-xl-5">
+
+                    <div id="image_wrapper">
+                        <img src="@/assets/images/about/about/About_kids.png" alt="">
+                    </div>
+
+                </div>
+
+            </div>
+
+
+        </div>
+
+    </section>
+</template>
+
+<script>
+export default {
+    name: 'About',
+}
+</script>
+
+<style lang="scss" scoped>
+#about {
+
+    margin-top: 72px;
+
+    padding-top: 40px;
+    padding-bottom: 40px;
+
+    .container {
+
+        #text_wrapper {
+
+            h3 {
+                color: $Mov;
+                // text-align: right;
+                font-size: 32px;
+                // font-weight: 500;
+
+                @include breakpoints(x-small) {
+                    font-size: 24px;
+                    text-align: center;
+                }
+
+                @include breakpoints(small) {
+                    font-size: 24px;
+                    text-align: center;
+                }
+
+                @include breakpoints(medium) {
+                    font-size: 24px;
+                }
+
+                @include breakpoints(large) {
+                    font-size: 24px;
+                }
+            }
+
+            h5 {
+                color: $Green;
+                font-size: 18px;
+                // font-weight: 500;
+            }
+
+            p {
+                color: $Black;
+                font-size: 18px;
+                font-weight: 400;
+            }
+
+        }
+
+        #image_wrapper {
+
+            width: 529px;
+            height: 392px;
+
+            @include breakpoints(x-small) {
+                width: 343px;
+                height: 254px;
+                margin: 0 auto;
+            }
+
+            @include breakpoints(small) {
+                width: 343px;
+                height: 254px;
+                margin: 0 auto;
+            }
+
+            @include breakpoints(medium) {
+                width: 324px;
+                height: 235px;
+            }
+
+            @include breakpoints(large) {
+                width: 343px;
+                height: 254px;
+            }
+
+            img {
+                width: 100%;
+                height: 100%;
+                object-fit: cover;
+            }
+
+        }
+
+    }
+
+}
+</style>
--- src/views/AboutView.vue
+++ src/views/AboutView.vue
@@ -1,13 +1,42 @@
 <template>
-    <h1>About Page</h1>
+    <div id="about_page">
+
+        <!-- Start About -->
+        <About />
+        <!-- End About -->
+
+        <!-- Start Footer -->
+        <Footer />
+        <!-- End Footer -->
+
+    </div>
 </template>
 
 <script>
+import About from '@/components/about/About.vue';
 export default {
-    name:'AboutView'
+    name: 'AboutView',
+    components: {
+        About,
+    }
 }
 </script>
 
 <style scoped lang="scss">
+#about_page {
 
+    // https://www.w3schools.com/cssref/pr_background-image.php
+    /* The image used */
+    background-image: url("@/assets/images/about/about_BG.jpg");
+
+    /* Used if the image is unavailable */
+    background-color: $White;
+    /* Center the image */
+    background-position: center;
+    /* Repeat the image */
+    background-repeat: repeat;
+    /* Resize the background image to cover the entire container */
+    background-size: cover;
+
+}
 </style>
